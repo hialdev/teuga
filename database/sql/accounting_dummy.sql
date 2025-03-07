8262881f27cccbf564eb463186a348acde04ff4b
unknown <al.developer12@gmail.com>
2025-03-07 10:13:49 +0700

    update sql accounting dummy

--- database/sql/accounting_dummy.sql
+++ database/sql/accounting_dummy.sql
@@ -29,9 +29,13 @@ REPLACE INTO `accounts` (`id`, `master_account_id`, `parent_account_id`, `code`,
 	('3d305aa5-6862-4570-879b-a7cc4a191fd1', 'de3c04bc-4474-4e52-8c3c-e2b0089605ae', NULL, '105', 'Surplus Revaluasi Aset', 'Pertambahan nilai yang terjadi saat Revaluasi Aset', 1, '2025-02-24 08:27:07', '2025-02-24 08:27:07'),
 	('536b8ddd-398b-436e-8a9c-8330c5e283df', 'ed1848ca-f2da-4826-93db-0ded5986bd67', NULL, '101', 'Hutang Pengangkutan (Logistik)', 'Akun Hutang Pengangkutan (Logistik)', 1, '2025-02-17 17:32:36', '2025-02-17 17:32:36'),
 	('542ef3dc-64c5-4822-a99a-ffac578524b8', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', '677163b9-295a-4cac-8045-fe8694392fd8', '100', 'PT Bangun Jaya Abadi', 'Akun Piutang untuk PT Bangun Jaya Abadi', 0, '2025-02-17 15:19:25', '2025-02-17 15:19:25'),
+	('571f5a37-7214-4c8f-8c51-46e690c2b407', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', '6c2e68c1-7d6a-485f-a56a-3436dc13e311', '101', 'Kendaraan', NULL, 0, '2025-02-28 07:49:04', '2025-02-28 07:49:04'),
+	('5e851af8-3a16-4a99-a061-3783f85a9664', 'ed1848ca-f2da-4826-93db-0ded5986bd67', NULL, '104', 'Hutang Pajak', 'Pajak yang Harus Dibayarkan', 1, '2025-03-01 08:44:52', '2025-03-01 08:44:52'),
 	('63d1c105-1406-46b2-a628-00e7ea6e2c43', 'ed1848ca-f2da-4826-93db-0ded5986bd67', 'ae4230dd-34bd-48df-bfc0-0702e8119fd9', '100', 'PT. Konstruksi Nusantara', 'Akun Hutang untuk PT. Konstruksi Nusantara', 0, '2025-02-17 16:56:25', '2025-02-17 16:56:25'),
+	('64f31390-abaa-4030-95bd-c1dd66b5eace', 'ed1848ca-f2da-4826-93db-0ded5986bd67', '536b8ddd-398b-436e-8a9c-8330c5e283df', '101', 'Logistic Special', 'Akun Hutang Logistik untuk Logistic Special', 0, '2025-03-01 07:37:26', '2025-03-01 07:37:26'),
 	('677163b9-295a-4cac-8045-fe8694392fd8', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', NULL, '103', 'Piutang (Receivable)', NULL, 1, '2025-02-12 05:21:48', '2025-02-12 05:21:48'),
 	('6c2e68c1-7d6a-485f-a56a-3436dc13e311', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', NULL, '102', 'Aset Tetap (Fixed Asset)', NULL, 1, '2025-02-12 05:20:47', '2025-02-12 05:20:47'),
+	('755c83cb-470f-4164-99a5-81e8780eed98', '4f7e3d6f-f6e2-41ad-9c6a-cf9e6bd9dccc', '260c3335-3453-497b-ac79-357ff555931b', '102', 'PT Nur Alif', 'Akun COGS/HPP untuk PT Nur Alif', 0, '2025-03-06 01:49:14', '2025-03-06 01:49:14'),
 	('76dd71f4-e1df-484f-82dc-547cd8adbd5f', 'de3c04bc-4474-4e52-8c3c-e2b0089605ae', NULL, '100', 'Laba Ditahan', NULL, 1, '2025-02-12 05:25:10', '2025-02-12 05:25:10'),
 	('79204556-ec2f-420f-9d0e-8a261b84060e', 'de3c04bc-4474-4e52-8c3c-e2b0089605ae', NULL, '101', 'Modal Pemilik', NULL, 1, '2025-02-12 05:25:23', '2025-02-12 05:25:23'),
 	('7bb7912c-4722-491a-809d-558e5aa1c132', '7c2f3c19-563d-47a4-bd64-08098d25ba1e', 'cfac6288-52c2-41c3-b4e7-0a234ed2815d', '100', 'CV Mandiri Perkasa', 'Akun Pendapatan untuk CV Mandiri Perkasa', 0, '2025-02-17 15:40:56', '2025-02-17 15:40:56'),
@@ -47,21 +51,20 @@ REPLACE INTO `accounts` (`id`, `master_account_id`, `parent_account_id`, `code`,
 	('a84cc8b8-0889-4086-9a82-58e13570832e', 'de3c04bc-4474-4e52-8c3c-e2b0089605ae', '82a91758-742b-4e1b-a019-a5ffb623acff', '100', 'Penyesuaian / Koreksi Saldo', NULL, 1, '2025-02-14 07:30:55', '2025-02-14 07:30:55'),
 	('ae4230dd-34bd-48df-bfc0-0702e8119fd9', 'ed1848ca-f2da-4826-93db-0ded5986bd67', NULL, '100', 'Hutang (Payable)', NULL, 1, '2025-02-12 05:24:47', '2025-02-12 05:24:47'),
 	('b4a779f5-d0d7-4a59-b60b-34d13ae0edc4', 'de3c04bc-4474-4e52-8c3c-e2b0089605ae', '82a91758-742b-4e1b-a019-a5ffb623acff', '101', 'Cadangan Modal Khusus', NULL, 1, '2025-02-14 07:38:11', '2025-02-14 07:38:11'),
+	('bdae4175-a046-4cec-b299-10395a4a25fc', 'ed1848ca-f2da-4826-93db-0ded5986bd67', '2e7a9f45-e814-4be6-8648-46a05d95a9d5', '102', 'PT Nur Alif', 'Akun Hutang untuk PT Nur Alif', 0, '2025-03-06 01:49:14', '2025-03-06 01:49:14'),
 	('c38b9714-ad9d-40c1-b6e4-92febf1e0f3c', 'ed1848ca-f2da-4826-93db-0ded5986bd67', '2e7a9f45-e814-4be6-8648-46a05d95a9d5', '101', 'PT. Konstruksi Nusantara', 'Akun Hutang untuk PT. Konstruksi Nusantara', 0, '2025-02-24 02:59:05', '2025-02-24 02:59:05'),
+	('c74641f1-d23a-4df8-9487-d55cf7562fc4', '7c2f3c19-563d-47a4-bd64-08098d25ba1e', '7bb7912c-4722-491a-809d-558e5aa1c132', '102', 'PT Bangun Jaya Abadi', 'Akun Pendapatan untuk PT Bangun Jaya Abadi', 0, '2025-03-06 01:43:56', '2025-03-06 01:43:56'),
 	('cfac6288-52c2-41c3-b4e7-0a234ed2815d', '7c2f3c19-563d-47a4-bd64-08098d25ba1e', 'e86d811f-0176-49e8-b019-213115922411', '100', 'PT Bangun Jaya Abadi', 'Akun Pendapatan untuk PT Bangun Jaya Abadi', 0, '2025-02-17 15:19:25', '2025-02-17 15:19:25'),
 	('d0b62525-1e74-44fd-9a0e-13f1694b64d6', '4f7e3d6f-f6e2-41ad-9c6a-cf9e6bd9dccc', '260c3335-3453-497b-ac79-357ff555931b', '101', 'PT. Beton Jaya Abadi', 'Akun COGS/HPP untuk PT. Beton Jaya Abadi', 0, '2025-02-19 09:42:04', '2025-02-19 09:42:04'),
+	('d985f325-b1fc-4149-be53-b3e17ea0d7d1', '4f7e3d6f-f6e2-41ad-9c6a-cf9e6bd9dccc', '0ba1d611-1541-4a4a-bd82-66f472eefbde', '101', 'Logistic Special', 'Akun Beban Logistik untuk Logistic Special', 0, '2025-03-01 07:37:26', '2025-03-01 07:37:26'),
 	('dee006b5-0fa6-450d-af76-8249c22369b1', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', 'ed2b3072-d039-493d-9bed-42abb045eabf', '100', 'Bank Central Asia (BCA)', NULL, 0, '2025-02-23 14:19:20', '2025-02-23 14:19:20'),
 	('e86d811f-0176-49e8-b019-213115922411', '7c2f3c19-563d-47a4-bd64-08098d25ba1e', NULL, '100', 'Pendapatan Penjualan', NULL, 1, '2025-02-12 05:26:27', '2025-02-12 05:26:27'),
 	('eb345412-b2b8-43b5-865b-1bdc6f740649', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', NULL, '104', 'Akumulasi Penyusutan', NULL, 1, '2025-02-12 05:22:08', '2025-02-12 05:28:05'),
 	('ed2b3072-d039-493d-9bed-42abb045eabf', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', NULL, '101', 'Bank', NULL, 1, '2025-02-12 05:06:40', '2025-02-12 05:16:54'),
 	('edabf990-1389-4642-ba1c-9ae2248496be', '4f7e3d6f-f6e2-41ad-9c6a-cf9e6bd9dccc', '260c3335-3453-497b-ac79-357ff555931b', '100', 'PT. Konstruksi Nusantara', 'Akun COGS/HPP untuk PT. Konstruksi Nusantara', 0, '2025-02-17 16:56:25', '2025-02-17 16:56:25'),
 	('ee1333a2-245c-4be0-9972-10da7cb3a07f', '4f7e3d6f-f6e2-41ad-9c6a-cf9e6bd9dccc', NULL, '103', 'Beban Penyusutan', NULL, 1, '2025-02-12 05:27:48', '2025-02-12 05:27:48'),
+	('ee972f9c-d546-4b76-b0b2-9831807fb029', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', '6c2e68c1-7d6a-485f-a56a-3436dc13e311', '100', 'Bangunan Permanen', NULL, 0, '2025-02-28 04:14:03', '2025-02-28 04:16:17'),
 	('f5a45e28-1c48-440f-9ccc-c95da50b6d5d', '34b24c62-78c9-476b-ac72-3eb2fe1a4bb2', NULL, '106', 'PPN Dibayar di Muka (Aset Pajak)', 'PPN Dibayar di Muka (Aset Pajak) hanya muncul saat PPN Masukan lebih besar daripada PPN Keluaran', 1, '2025-02-24 07:02:07', '2025-02-24 07:02:07');
-
--- Dumping data for table teuga_accounting.banks: ~2 rows (approximately)
-REPLACE INTO `banks` (`id`, `account_id`, `bank_name`, `logo`, `account_number`, `account_name`, `description`, `created_at`, `updated_at`) VALUES
-	('40c9a42a-211c-4723-82a3-d8d1dda03f63', NULL, 'Bank Central Asia (BCA)', 'banks/rjIsh0jJ0rrI1PWv7IrUz2CypzNschOXE8JTxw01.webp', 123456789, 'Muhammad Nur Alif', NULL, '2025-02-24 04:31:11', '2025-02-24 04:31:11'),
-	('42c6b19a-cb2f-4ba3-b0a3-1329fa784436', NULL, 'Bank Nasional Indonesia (BNI)', NULL, 123456789009876, 'PT Rizq Sahara Multindo', NULL, '2025-02-24 04:33:06', '2025-02-24 04:33:06');
 
 -- Dumping data for table teuga_accounting.master_accounts: ~5 rows (approximately)
 REPLACE INTO `master_accounts` (`id`, `code`, `type`, `created_at`, `updated_at`) VALUES
