754a3de7455f0ceeabab3105d3c11b8c97a3340b
unknown <al.developer12@gmail.com>
2025-03-08 12:22:33 +0700

    update migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        \Laravel\Sanctum\Sanctum::ignoreMigrations();
     }
 
     /**
