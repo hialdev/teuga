09adf4986feffaaf4c167f8615ebb9acba276efa
unknown <al.developer12@gmail.com>
2025-03-12 13:06:08 +0700

    upadted delivery null

--- resources/views/pdf/partial.blade.php
+++ resources/views/pdf/partial.blade.php
@@ -131,7 +131,7 @@ table.second-table>thead,table.second-table>tbody{
                     <td width="10" class="pl-1 pr-1">:</td>
                     <td>{{$invoice->requestOrder->no_refrence}}</td>
                 </tr>
-                @if($invoice->purchaseOrder->delivery)
+                @if(isset($invoice->purchaseOrder->delivery))
                 <tr>
                     <th width="90">Project Location</th>
                     <td width="10" class="pl-1 pr-1">:</td>
